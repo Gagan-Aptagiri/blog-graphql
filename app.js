2227dd2a7325f6ff5999777ed98d4a9fc6aa3d2a
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-03-31 20:58:57 +0530

    Added Image upload functionality

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
@@ -56,6 +57,21 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
+app.put('/post-image', (req, res, next) => {
+	if (!req.isAuth) {
+		throw new Error('Not authenticated.');
+	}
+	if (!req.file) {
+		return res.status(200).json({ message: 'No file provided.' });
+	}
+	if (req.body.oldPath) {
+		clearImage(req.body.oldPath);
+	}
+	return res
+		.status(201)
+		.json({ message: 'File stored.', filePath: req.file.path });
+});
+
 app.use(
 	'/graphql',
 	graphqlHTTP({
@@ -101,3 +117,8 @@ mongoose
 		});
 	})
 	.catch((err) => console.log(err));
+
+const clearImage = (filepath) => {
+	filepath = path.join(__dirname, '..', filePath);
+	fs.unlink(filePath, (err) => console.log(err));
+};
