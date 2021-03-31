d72679f008acf59c5592d31f976d3440d40a3810
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-03-31 18:52:18 +0530

    Get posts resolver added

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -106,11 +106,34 @@ module.exports = {
 		});
 		const createdPost = await post.save();
 		user.posts.push(createdPost);
+		await user.save();
 		return {
 			...createdPost._doc,
 			_id: createdPost._id.toString(),
 			createdAt: createdPost.createdAt.toISOString(),
 			updatedAt: createdPost.updatedAt.toISOString(),
+		};
+	},
+
+	posts: async function (args, req) {
+		if (!req.isAuth) {
+			const error = new Error('Not authenticated.');
+			error.code = 401;
+			throw error;
+		}
+		const totalPosts = await Post.find().countDocuments();
+		const posts = await Post.find().sort({ createdAt: -1 }).populate('creator');
+
+		return {
+			posts: posts.map((p) => {
+				return {
+					...p._doc,
+					_Id: p._id.toString(),
+					createdAt: p.createdAt.toISOString(),
+					updatedAt: p.updatedAt.toISOString(),
+				};
+			}),
+			totalPosts: totalPosts,
 		};
 	},
 };
