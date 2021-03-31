afc4b34efb025962520995a11c5f5fb28386e460
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-03-31 14:46:38 +0530

    Added create post resolver

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,6 +3,7 @@ const validator = require('validator');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+const Post = require('../models/post');
 
 module.exports = {
 	createUser: async function ({ userInput }, req) {
@@ -41,7 +42,7 @@ module.exports = {
 	},
 
 	login: async function ({ email, password }) {
-		const user = await User.findOne('email');
+		const user = await User.findOne({ email: email });
 		if (!user) {
 			const error = new Error('User not found.');
 			error.code = 401;
@@ -62,5 +63,40 @@ module.exports = {
 			{ expiresIn: '1h' },
 		);
 		return { token: token, userId: user._id.toString() };
+	},
+
+	createPost: async function ({ postInput }, req) {
+		const errors = [];
+		if (
+			validator.isEmpty(postInput.title) ||
+			!validator.isLength(postInput.title, { min: 5 })
+		) {
+			errors.push({ message: 'Title is invalid.' });
+		}
+		if (
+			validator.isEmpty(postInput.content) ||
+			!validator.isLength(postInput.content, { min: 5 })
+		) {
+			errors.push({ message: 'content is invalid.' });
+		}
+		if (errors.length > 0) {
+			const error = new Error('Invalid input.');
+			error.data = errors;
+			error.code = 422;
+			throw error;
+		}
+		const post = new Post({
+			title: postInput.title,
+			content: postInput.content,
+			imageUrl: postInput.imageUrl,
+		});
+		const createdPost = await post.save();
+		//Add posts to user's posts
+		return {
+			...createdPost._doc,
+			_id: createdPost._id.toString(),
+			createdAt: createdPost.createdAt.toISOString(),
+			updatedAt: createdPost.updatedAt.toISOString(),
+		};
 	},
 };
