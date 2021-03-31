8ca2e917141bdb2077b50baa7428619c2e5c0801
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-03-31 18:35:39 +0530

    User and createpost relation set up along with authentication

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { graphqlHTTP } = require('express-graphql');
 
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
+const auth = require('./middleware/auth');
 
 const app = express();
 
@@ -52,6 +53,8 @@ app.use((req, res, next) => {
 	}
 	next();
 });
+
+app.use(auth);
 
 app.use(
 	'/graphql',
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -66,6 +66,11 @@ module.exports = {
 	},
 
 	createPost: async function ({ postInput }, req) {
+		if (!req.isAuth) {
+			const error = new Error('Not authenticated.');
+			error.code = 401;
+			throw error;
+		}
 		const errors = [];
 		if (
 			validator.isEmpty(postInput.title) ||
@@ -85,13 +90,22 @@ module.exports = {
 			error.code = 422;
 			throw error;
 		}
+
+		const user = await User.findById(req.userId);
+		if (!user) {
+			const error = new Error('Invalid User.');
+			error.code = 401;
+			throw error;
+		}
+
 		const post = new Post({
 			title: postInput.title,
 			content: postInput.content,
 			imageUrl: postInput.imageUrl,
+			creator: user,
 		});
 		const createdPost = await post.save();
-		//Add posts to user's posts
+		user.posts.push(createdPost);
 		return {
 			...createdPost._doc,
 			_id: createdPost._id.toString(),
